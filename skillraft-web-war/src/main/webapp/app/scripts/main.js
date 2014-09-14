c439867f30a4c8f680c830cb7e1415371ccf3cbb
Sudhanva Gnaneshwar <sudhanva.gn@gmail.com>
2014-09-14 01:59:09 -0700

    Yeomon integration

---
+++ skillraft-web-war/src/main/webapp/app/scripts/main.js
@@ -0,0 +1,23 @@
+/*global require*/
+'use strict';
+
+require.config({
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        }
+    },
+    paths: {
+        jquery: '../vendor/jquery/dist/jquery',
+        backbone: '../vendor/backbone/backbone',
+        underscore: '../vendor/lodash/dist/lodash',
+        bootstrap: '../vendor/sass-bootstrap/dist/js/bootstrap'
+    }
+});
+
+require([
+    'backbone'
+], function (Backbone) {
+    Backbone.history.start();
+});
